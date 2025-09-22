b3bc386fd626d07e42e635fe1e33c060f669cf58
Shawheen Naderi <shawheennaderi@campus-049-224.ucdavis.edu>
2025-09-22 15:35:51 -0700

    buffer_wrapper change

--- offline/data/buffer_wrapper.py
+++ offline/data/buffer_wrapper.py
@@ -8,14 +8,14 @@ class NPZReplayBuffer(ReplayBuffer):
         self.action_dim = action_dim
 
     def push_batch(self, obs, actions, rewards, dones, next_obs):
-        """Push a batch of transitions into the replay buffer."""
-        batch_size = len(obs)
-        for i in range(batch_size):
-            self.add(
-                obs[i],
-                actions[i],
-                rewards[i],
-                dones[i],
-                next_obs[i],
-                info={}
-            )
+        """Push a batch of transitions into the replay buffer."""       
+        data = Batch(
+        obs=obs,
+        act=actions,
+        rew=rewards,
+        done=dones,
+        obs_next=next_obs,
+        info={}  # now correctly nested inside Batch
+    )
+    self.add(data)  # pass Batch instead of individual keyword args
+
