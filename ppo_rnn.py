c06265263210955767ec2a85076be83b21f77c66
Shawheen Naderi <shawheennaderi@Shawheens-MacBook-Air.local>
2026-01-25 13:55:43 -0800

    RNN AC dim fix

--- ppo_rnn.py
+++ ppo_rnn.py
@@ -82,7 +82,7 @@ class ActorCriticRNN(nn.Module):
         
         x_enc = nn.relu(x_enc)
 
-        embedding = x_enc.reshape(x_enc.shape[0], x_enc.shape[1], -1)
+        z = x_enc.reshape(x_enc.shape[0], x_enc.shape[1], -1)
 
         rnn_in = nn.Dense(256, kernel_init=orthogonal(2))(z)
         rnn_in = nn.relu(rnn_in)
