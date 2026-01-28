f1a3e9524bbbd525e673d312d9f45c2963544b80
Shawheen Naderi <shawheennaderi@Shawheens-MacBook-Air.local>
2026-01-27 16:34:17 -0800

    collect data

--- analysis/collect_data.py
+++ analysis/collect_data.py
@@ -40,8 +40,7 @@ def main():
         # Inference (Actor + Critic)
         obs_in = last_obs[:, None, :] 
         done_in = done_mask[:, None, None]
-        new_hstate, pi, value = network.apply(trained_params, hstate[None, :], (obs_in, done_in))
-        new_hstate = new_hstate.squeeze(0)
+        new_hstate, pi, value = network.apply(trained_params, hstate, (obs_in, done_in))
         
         # Action Sampling
         rng, action_rng = jax.random.split(rng)
